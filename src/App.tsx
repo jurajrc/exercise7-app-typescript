5b6d2d1a3dbe94d0b3e1adbbe4c092e9f044ffcb
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-30 17:36:28 +0100

    8. clearing App.tsx & index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Components
+import Home from './pages/Home';
+// Style
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Home />
+      </div>
+    </>
   );
 }
 
