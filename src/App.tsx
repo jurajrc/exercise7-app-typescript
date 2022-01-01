52fc156df24fc202980fa60804cfdd092d94e636
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-01 20:13:53 +0100

    18. implement Routers & Route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 // Components
 import Header from './components/Header';
 import Home from './pages/Home';
+import Destination from './pages/Destination';
+import Crew from './pages/Crew';
+import Technology from './pages/Technology';
 // Style
 import GlobalStyle from './components/GlobalStyle';
 
@@ -11,7 +15,14 @@ function App() {
       <GlobalStyle />
       <div className="App">
         <Header />
-        <Home />
+
+        <Routes>
+          <Route path="/" element= {<Home />} />
+          <Route path="/destination" element= {<Destination />} />
+          <Route path="/crew" element= {<Crew />} />
+          <Route path="/technology" element= {<Technology />} />
+        </Routes>
+        
       </div>
     </>
   );
