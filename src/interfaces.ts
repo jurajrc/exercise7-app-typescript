32de64b2650102575621e1e80cafa4a3a85af618
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-01 21:25:19 +0100

    20. creata interfaces.ts & type ItemData

---
+++ src/interfaces.ts
@@ -0,0 +1,10 @@
+export interface ItemData {
+    name: string;
+    images: {
+        png: string;
+        webp: string;
+    };
+    description: string;
+    distance: string;
+    travel: string;
+}[]
