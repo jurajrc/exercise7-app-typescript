ff3b49e2b53172f6bbfb2a02d354214424c7b664
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-06 20:14:38 +0100

    26.create styles.tsx

---
+++ src/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const Line = styled(motion.div)`
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 3px;
+    background: #fff;
+`
