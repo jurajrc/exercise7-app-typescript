86922b43fd78753715e13e6e21043cac2da7f877
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-06 20:20:51 +0100

    29. import SubNavDestination

--- src/components/TemplateDestination.tsx
+++ src/components/TemplateDestination.tsx
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 // Animatio
 import { motion } from 'framer-motion'
 import { ItemData } from '../interfaces'
+import SubNavDestination from './SubNavDestination'
 
 interface Props {
     itemDestination: ItemData
@@ -15,27 +16,22 @@ const TemplateDestination = ({ itemDestination }: Props) => {
 
     const {name, images, description, distance, travel } = itemDestination
     
+    
+    
     return (
         <StyleSection>
             <div className="left">
-                    <img src={images.webp} alt="moon" />
+                    <img src={images.webp} alt={name} />
                 </div>
                 <div className="right">
                     <div className="info">
-                        <div className="sbut-nav">
-                            <ul>
-                                <li>moon
-                                    <Line />
-                                </li>
-                                <li>mars</li>
-                                <li>europa
-                                    <Line />
-                                </li>
-                                <li>titan</li>
-                            </ul>
+                        <div className="sub-nav">
+                            <SubNavDestination />
                         </div>
                         <h1>{name}</h1>
-                        <p className='text'>{description}</p>
+                        <div className='text'>
+                            <p>{description}</p>
+                        </div>
                         <div className="parameters">
                             <div className="parameter">
                                 <p>Avg. distance</p>
@@ -57,6 +53,14 @@ const StyleSection = styled(motion.section)`
         display: flex;
         width: 100%;
         height: 72vh;
+        @media (max-width: 1000px) {
+            flex-direction: column;
+            height: 80vh;
+            align-items: center;
+        }
+        @media (max-width: 768px) {
+            height: 85vh;
+        }
 
         .left {
             width: 50%;
@@ -66,45 +70,97 @@ const StyleSection = styled(motion.section)`
 
             img {
                 margin-left: 20%;
+                max-width: 100%;
+                height: auto;
+            }
+            @media (max-width: 1000px) {
+                width: 100%;
+                img {
+                    margin: 2rem 0;
+                    width: 18.75em;
+                    @media (max-width: 500px) {
+                        width: 10.6em;
+                    }
+                }
             }
         }
         .right {
             width: 50%;
+            @media (max-width: 1000px) {
+                width: 100%;
+                
+            }
             .info {
                 width: 70%;
                 padding-left: 10%;
                 padding-top: 6%;
+                @media (max-width: 1150px) {
+                    width: 85%;
+                }
+                @media (max-width: 1000px) {
+                    width: 100%;
+                    padding: 0 10%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
             }
-             ul {
-                 padding: none;
-                 display: flex;
-                 width: 65%;
-                 justify-content: space-between;
-                 
-                 li {
-                     position: relative;
-                     padding: .6em 0;
-                     text-transform: uppercase;
-                     letter-spacing: 2.7px;
-                 }
-             }
+            
              h1 {
                  font-size: 6.25em;
                  text-transform: uppercase;
                  padding: 2.5rem 0;
-                 
+                 @media (max-width: 1000px) {
+                     font-size: 5em;
+                     padding: 1.5rem 0 1rem;
+                 }
+                 @media (max-width: 500px) {
+                     font-size: 3.5em;
+                     padding: 1.4rem 0 .3rem;
+                 }
              }
              .text {
                 padding-bottom: 2.5em;
                 border-bottom: 1px solid hsla(0, 0%, 100%, 0.25);
                 margin-bottom: 1em;
+                @media (max-width: 1000px) {
+                    width: 100%;
+                    font-size: 1em;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+                @media (max-width: 500px) {
+                    font-size: .93em;
+                }
              }
              .parameters {
+                 width: 100%;
                  display: flex;
+
+                 @media (max-width: 1000px) {
+                     justify-content: space-evenly;
+                 }
+                 @media (max-width: 500px) {
+                    flex-direction: column;
+                    align-items: center;
+                    
+                 }
 
                  .parameter {
                      width: 50%;
                      text-transform: uppercase;
+
+                     @media (max-width: 1000px) {
+                     width: auto;
+                    }
+                    @media (max-width: 500px) {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        padding-bottom: 1.5em;
+                    }
+                     
                      p {
                          font-size: 14px;
                          letter-spacing: 2.2px;
@@ -115,16 +171,17 @@ const StyleSection = styled(motion.section)`
                      }
                  }
              }
+             
         }
     
 `
-const Line = styled(motion.div)`
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 3px;
-    background: #fff;
-`
+// const Line = styled(motion.div)`
+//     position: absolute;
+//     left: 0;
+//     bottom: 0;
+//     width: 100%;
+//     height: 3px;
+//     background: #fff;
+// `
 
 export default TemplateDestination
