5d472e1cdddd4a4788a97fab95aed4fd8e0331f5
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-06 20:24:39 +0100

    31. nesting router in /destination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // Components
 import Header from './components/Header';
@@ -6,10 +6,16 @@ import Home from './pages/Home';
 import Destination from './pages/Destination';
 import Crew from './pages/Crew';
 import Technology from './pages/Technology';
+// Data
+import data from './data.json'
 // Style
 import GlobalStyle from './components/GlobalStyle';
+import TemplateDestination from './components/TemplateDestination';
 
 function App() {
+
+  const [allData, setAllData] = useState(data)
+
   return (
     <>
       <GlobalStyle />
@@ -17,10 +23,16 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element= {<Home />} />
-          <Route path="/destination" element= {<Destination />} />
-          <Route path="/crew" element= {<Crew />} />
-          <Route path="/technology" element= {<Technology />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/destination/" element={<Destination />} >
+            <Route path="/destination/moon" element={<TemplateDestination itemDestination={allData.destinations[0]} />} />
+            <Route path="/destination/mars" element={<TemplateDestination itemDestination={allData.destinations[1]} />} />
+            <Route path="/destination/europa" element={<TemplateDestination itemDestination={allData.destinations[2]} />} />
+            <Route path="/destination/titan" element={<TemplateDestination itemDestination={allData.destinations[3]} />} />
+          </Route>
+
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
         </Routes>
         
       </div>
