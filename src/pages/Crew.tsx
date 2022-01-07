c7b13d8257ce2cd1a9f6a70986b55f042c8eb189
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-07 15:23:13 +0100

    41. media image back. Crew.tsx

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -1,35 +1,49 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+// Components
+import Heading from '../components/Heading'
+
 // Style
 import styled from 'styled-components'
 // Data
-import data from '../data.json'
+
 // Image
-import imageBackCrew from '../assets/crew/background-crew-desktop.jpg'
+import imageBackDesktop from '../assets/crew/background-crew-desktop.jpg'
+import imageBackTablet from '../assets/crew/background-crew-tablet.jpg'
+import imageBackMobile from '../assets/crew/background-crew-mobile.jpg'
 
 const Crew: React.FC = () => {
     return (
         <StyleCrew>
-            <h1>Crew</h1>
+            <Heading numSite='2' heading='Meet your crew' />
+            <Outlet />
         </StyleCrew>
     )
 }
 const StyleCrew = styled.article`
     width: 100%;
     min-height: 100vh;
-    background: url(${imageBackCrew}), #0b0d17;
+    background: url(${imageBackDesktop}), #0b0d17;
     background-blend-mode: difference;
     background-position: center;
     background-size: cover;
     background-attachment: fixed;
-
-    header {
-        width: 100%;
-        height: 28vh;
-        display: flex;
-        justify-content: flex-start;
-        align-items: flex-end;
-        
+    overflow: hidden;
+    @media (max-width: 768px) {
+        background: url(${imageBackTablet}), #0b0d17;
+        background-position: center;
+        background-size: cover;
+        overflow: visible;
+        overflow-x: hidden;
     }
+    @media (max-width: 450px) {
+        background: url(${imageBackMobile}), #0b0d17;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+
+    
 `
 
 export default Crew
