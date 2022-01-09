f42e7587c09536e6e2893b32bf0c8985252dedf8
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-01-09 23:30:08 +0100

    62. create animations.ts

---
+++ src/animations.ts
@@ -0,0 +1,14 @@
+export const pageAnimate = {
+    hidden: {
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            duration: .5
+        }
+    },
+    exit: {
+        opacity: 0
+    }
+}
